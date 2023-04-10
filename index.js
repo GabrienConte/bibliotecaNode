c49eee0771b7ef7063bc931747940b52521bb2d1
GabrienConte <gabrielconte0@gmail.com>
2023-04-10 20:47:46 -0300

    método getArchive created

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+import fs from 'fs';
 import chalk from "chalk";
 
-console.log(chalk.green('olá mundo'));
+function trataErro(error){
+    throw new Error(chalk.red(error.code, 'Não há arquivo no diretório'));
+}
+
+async function getArchive(pathfile) {
+    const enconding = 'utf-8';
+    try {
+        const text = await fs.promises.readFile
+        (pathfile, enconding);
+        console.log(chalk.greenBright(text));
+    } catch (error) {
+        trataErro(error);
+    } finally {
+        console.log(chalk.grey('Operação Concluída'));
+    }
+
+}
+
+// function getArchive(pathfile) {
+//     const enconding = 'utf-8';
+//     fs.promises
+//       .readFile(pathfile, enconding)
+//       .then((text) => console.log(chalk.greenBright(text)))
+//       .catch(trataErro);
+// }
+
+getArchive('./arquivos/texto.md');
